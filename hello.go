e0016030a2b89af3303078633734437e7a5f376b
pkryvyi <kryvyi.petro@gmail.com>
2018-06-20 15:53:55 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,5 +1,21 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
 func main() {
-    fmt.Println("hello world")
+	http.HandleFunc("/", handle)
+	appengine.Main()
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
